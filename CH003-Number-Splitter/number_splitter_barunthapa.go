07ec324531d47276ccb87c7c5ce501b9a3bf7cde
barunthapa <barunthapa.bvdt@gmail.com>
2019-10-30 23:47:27 +0545

    Support 0 value.

--- CH003-Number-Splitter/number_splitter_barunthapa.go
+++ CH003-Number-Splitter/number_splitter_barunthapa.go
@@ -11,18 +11,37 @@ func split(n, x int) []int {
 	var nums []int
 	on := n
 	sum := 0
-	s1 := rand.NewSource(time.Now().UnixNano())
-    r1 := rand.New(s1)
 	for i := x; i > 1; i-- {
-		r := r1.Intn(n - x)
+		if n <= x {
+			nums = append(nums, 1)
+			continue
+		}
+		r := getRand(n - x)
 		nums = append(nums, r)
 		sum = sum + r
 		n = n - r
 	}
+	fmt.Println("Sum:", sum)
 	nums = append(nums, on-sum)
 	return nums
 }
 
 func main() {
-	fmt.Println(split(10, 3))
+	fmt.Println(split(100, 5))
+}
+
+func getRand(num int) int {
+	fmt.Println("num", num)
+	if num < 1 {
+		return 0
+	}
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+	n := r.Intn(num)
+	fmt.Println(n)
+	for ; n == 0 ; {
+		n = r.Intn(num + 1)
+		fmt.Println(n)
+	}
+	return n
 }
